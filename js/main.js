47a80b018d88c9f41dad4ad6b03ed3c70a5df976
= <=>
2023-05-24 14:59:15 +0530

    f

--- js/main.js
+++ js/main.js
@@ -3,48 +3,64 @@ onload = () => {
   let container = document.getElementById("nav-container")
   let main_container = document.getElementById("main-nav-container")
   let cancel_btn = document.getElementById("cancel-button")
+  let main_content = document.getElementById("main-content")
   let menu_state = false
   menu_button.onclick = () => {
-    container.style.display = "flex";
-    main_container.style.width = "40%"
+    console.log(menu_state)
+    container.style.width = "40%"
     menu_state = false
-    menu_button.style.display = "none"
-    cancel_btn.style.display = "flex"
+    menu_button.style.visibility = "hidden"
+    cancel_btn.style.visibility = "visible";
   }
   cancel_btn.onclick = () => {
-    cancel_btn.style.display = "none"
-    menu_button.style.display="flex"
-    container.style.display = "none";
+    cancel_btn.style.visibility = "hidden"
+    menu_button.style.visibility="visible"
+    container.style.width = "0";
     menu_state = true
   }
   window.onresize = () => {
     if (window.innerWidth >= 800) {
-      container.style.display = "flex";
-      main_container.style.width = "100%"
-      cancel_btn.style.display = "none"
-      menu_button.style.display = "none"
+
+      container.style.width = "100%"
+      menu_button.style.visibility = "hidden"
+      cancel_btn.style.visibility = "hidden";
     }
       else if(window.innerWidth < 800) {
+      container.style.width = "40%"
         if(menu_state) {
 
-          container.style.display = "none";
-          main_container.style.width="6.4rem"
+          // container.style.display = "none";
+          // main_container.style.width="100%"
           // menu_state = false
 
-          cancel_btn.style.display = "none"
-          menu_button.style.display="flex"
+          container.style.width = 0;
+
+          cancel_btn.style.visibility = "hidden"
+          menu_button.style.visibility= "visible"
         }
         else {
-          menu_button.style.display = "none"
-          cancel_btn.style.display = "flex"
-          container.style.display = "flex";
-          main_container.style.width = "40%"
+          container.style.width = "40%"
+          menu_button.style.visibility= "hidden"
+          cancel_btn.style.visibility= "visible"
+          // container.style.display = "flex";
+          // main_container.style.width = "40%"
           // menu_state = true
         }
       }
     }
+    main_content.onclick = () => {
+
+    if(!menu_state) {
+
+      cancel_btn.style.visibility = "hidden"
+      menu_button.style.visibility= "visible"
+      container.style.width = "0";
+      menu_state = true
+    }
+
+    }
+
   }
-  let main_div = document.getElementById("main-div")
 
 
 
